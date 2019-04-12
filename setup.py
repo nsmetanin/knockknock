b687e1df7fc00a7a4cfff3ca68648318013b9aca
Nikita Smetanin <nikitozzz.pl@gmail.com>
2019-04-12 17:42:09 +0300

    Python 3.5 support

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
 	license='MIT',
 	packages=find_packages(),
 	zip_safe=False,
-	python_requires='>=3.6',
+	python_requires='>=3.5',
 	install_requires=[
 		'yagmail>=0.11.214',
 		'keyring',
